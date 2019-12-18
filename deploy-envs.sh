d522741c78b03cbd52329f9ea116c0c6a8bb0764
Andrea Simeone <andreasimeone@gmail.com>
2019-12-18 15:17:36 -0600

    added let's encrypt test

--- deploy-envs.sh
+++ deploy-envs.sh
@@ -6,7 +6,9 @@ export IMAGE_VERSION=latest
 
 export AWS_DEFAULT_REGION=us-east-1
 export AWS_ECS_CLUSTER_NAME=default
-export AWS_VIRTUAL_HOST=redisgeo.netcore.io
+export AWS_VIRTUAL_HOST=groovejones.andreasimeone.com
+export LETSENCRYPT_HOST=$AWS_VIRTUAL_HOST
+export LETSENCRYPT_EMAIL=andreasimeone@gmail.com
 
 # set any sensitive information in travis-ci encrypted project settings:
 # required: AWS_ACCOUNT_NUMBER, AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY
